a9953c1cd582ac87051154368f52b234fcf5bfa8
Ian Kluft <ian.kluft+github@gmail.com>
2022-11-02 22:04:21 -0700

    back off 010_fetch_reqs.t until it works from both desktop CLI and container environment (GH-1) - update share/sys-ospkg-test directory with container test tool used to investigate CPAN Testers' breakage report

--- main/share/sys-ospkg-test/container-startup.sh
+++ main/share/sys-ospkg-test/container-startup.sh
@@ -4,7 +4,7 @@
 
 # function to print error message and exit
 die() {
-    echo "error: $*" >&2
+    echo "container error: $*" >&2
     exit 1
 }
 
@@ -15,9 +15,10 @@ then
 fi
 
 # set environment variables for build
-TMPDIR="logs/$SYS_OSPKG_TIMESTAMP"
+TMPDIR="/opt/container/logs/$SYS_OSPKG_TIMESTAMP"
+HOME="$TMPDIR"
 SYS_OSPACKAGE_DEBUG=1
-export TMPDIR SYS_OSPACKAGE_DEBUG
+export TMPDIR HOME SYS_OSPACKAGE_DEBUG
 
 # check if container filesystem is set up
 if [ ! -d "$TMPDIR" ]
@@ -30,36 +31,40 @@ if [ "$num_tarballs" -eq 0 ]
 then
     die "Sys-OsPackage tarball file required: not found"
 fi
-if [ "$num_tarballs" -gt 1 ]
-then
-    die "Sys-OsPackage tarball file required: more than one found"
-fi
-tarball="$(ls -1 Sys-OsPackage-*.tar.gz)"
+# shellcheck disable=SC2012
+tarball="$(ls -t Sys-OsPackage-*.tar.gz | head -1)"
 srcdir="$(basename "$tarball" .tar.gz)"
 
 # unpack Sys::OsPackage
 tar -xf "$tarball"
-echo "working directory contents:"
-tree -aC
+if [ -n "$SYS_OSPKG_TEST_CONTENTS" ]
+then
+    echo "working directory contents:"
+    echo "id in container: $(id)"
+    tree -aC
+    echo "home=$HOME"
+    ls -laZ "$HOME"
+else
+    echo "home=$HOME"
+fi
 echo
+echo "cd to $srcdir"
 cd "$srcdir" || die "cd to $srcdir failed"
 
-# install prerequisites from CPAN
-missing_authordeps="$(dzil authordeps --missing)"
-if [ -s "$missing_authordeps" ]
+# interactive shell to inspect environment, if requested
+if [ -n "$SYS_OSPKG_TEST_INTERACTIVE" ]
 then
-    echo "installing missing author dependencies: $missing_authordeps"
-    # shellcheck disable=SC2086
-    cpanm $missing_authordeps || die install author deps failed
-fi
-missing_deps="$(dzil listdeps --missing)"
-if [ -s "$missing_deps" ]
-then
-    echo "installing missing dependencies: $missing_deps"
-    # shellcheck disable=SC2086
-    cpanm $missing_deps || die install author deps failed
+    bash -il -o vi
 fi
 
+# install dependencies
+echo install dependencies with cpanm
+cpanm --installdeps .
+
 # build Sys::OsPackage and run tests
-dzil build || die build failed
-dzil test || die test failed
+echo build
+perl Build.PL || die Build.PL failed
+perl Build || die build failed
+echo run test
+perl Build test || die test failed
+exit $?
